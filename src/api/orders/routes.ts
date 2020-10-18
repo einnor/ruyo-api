aff63aa0d8ab61778936d8e04f8ae8d5cb4b4dae
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-10-18 15:23:53 +0300

    Added the routes

---
+++ src/api/orders/routes.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import * as paramValidation from './paramValidation';
+import * as methods from './index';
+import { validateResults } from 'middlewares';
+
+const router = express.Router();
+
+router.route('/').get(paramValidation.list, validateResults, methods.list);
+
+router
+  .route('/:id')
+  .get(paramValidation.getOrderById, validateResults, methods.getOrderById)
+  .post(paramValidation.update, validateResults, methods.update);
+
+export default router;
